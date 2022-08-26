76012d8acab6034c252d97617eced04799e3bb97
jgckM <z3515235474@163.com>
2022-08-26 11:04:50 +0800

    添加操作通知反馈

--- src/components/editMenu.vue
+++ src/components/editMenu.vue
@@ -1,38 +1,56 @@
 <template>
   <div class="menu">
-    <edit-warm :is-show="warnShow" @changeShow="changeShow"></edit-warm>
+    <div>
+      <edit-loading v-if="isShow"></edit-loading>
+      <edit-warn :is-show="warnShow" @changeShow="changeShow"></edit-warn>
+      <edit-success
+        v-if="successShow"
+        :isShow="successShow"
+        @changeShow="changeSuccessShow"
+        :text="successText"
+      ></edit-success>
+    </div>
     <button @click="handleClick">⇈推送源</button>
     <button @click="pull">⇊拉取源</button>
-    <button @click="handleClick">⋘编辑源</button>
     <button @click="handleClick">⋙生成源</button>
     <button @click="clearEdit">✗清空表单</button>
     <button @click="handleClick">↶撤销操作</button>
     <button @click="handleClick">↷重做操作</button>
     <button @click="handleClick">⇏调试源</button>
     <button @click="handleClick">✓保存源</button>
-    <edit-loading v-if="isShow"></edit-loading>
   </div>
 </template>
 
 <script>
 import { http } from "@/utils/http";
 import { ref } from "vue";
-
 import store from "@/store";
-import editLoading from "@/components/editLoading";
-import editWarm from "@/components/editWarm";
+
+import editLoading from "@/components/message/editLoading";
+import editWarn from "@/components/message/editWarn";
+import editSuccess from "@/components/message/editSuccess";
 
 export default {
-  components: { editLoading, editWarm },
+  components: { editLoading, editWarn, editSuccess },
   setup() {
-    const isShow = ref(false);
-    const warnShow = ref(false);
+    const isShow = ref(false); // loading
+    const warnShow = ref(false); // warn^
+    const successText = ref(""); // successText
+    const successShow = ref(false); // success
+
     const handleClick = () => {
       console.log(1111);
     };
+    // warnShow
     const changeShow = (bool) => {
       warnShow.value = bool;
     };
+    // successShow
+    const changeSuccessShow = (bool) => {
+      console.log(bool);
+      successShow.value = bool;
+    };
+
     const pull = () => {
       isShow.value = true;
       console.log(store.state.url);
@@ -41,6 +59,8 @@ export default {
           store.commit("changeSource", res.data);
           localStorage.setItem("url", store.state.url);
           isShow.value = false;
+          successText.value = `成功拉取${res.data.length}条源`;
+          successShow.value = true;
         })
         .catch((err) => {
           console.log(err);
@@ -52,6 +72,8 @@ export default {
     const clearEdit = () => {
       store.commit("clearEdit");
       console.log("已清除");
+      successText.value = "已清除";
+      successShow.value = true;
     };
 
     return {
@@ -61,6 +83,9 @@ export default {
       isShow,
       changeShow,
       warnShow,
+      successText,
+      changeSuccessShow,
+      successShow,
     };
   },
 };
