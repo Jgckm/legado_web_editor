4bd0a1ca38ed986c9e5d4cd68efd2f2145e7e294
jgckM <z3515235474@163.com>
2022-08-30 15:24:29 +0800

    update build.yml

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
-// module.exports = {
-//   publicPath: "././",
-//   outputDir: "dist",
-// };
+module.exports = {
+  publicPath: "././",
+  outputDir: "dist",
+};
