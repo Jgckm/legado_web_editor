f13d9605b9552adec7a494e3b34815ae2613d4b2
Xwite <1797350009@qq.com>
2022-09-22 13:12:28 +0800

    feat: adjust textarae height when focused and changed

--- src/components/editInput.vue
+++ src/components/editInput.vue
@@ -10,14 +10,15 @@
       ref="attr"
       @input="change"
       @blur="changeHistory();restoreHeight()"
-      @focus="changeHeight"
+      @focus="adjustHeight"
     ></textarea>
   </div>
 </template>
 
 <script>
-import { nextTick, ref } from "vue";
+import { ref } from "vue";
 import store from "@/store";
+import calcTextareaHeight from "@/utils/calcTextareaHeight";
 
 export default {
   props: {
@@ -55,6 +56,7 @@ export default {
         key: attr.value.getAttribute("id"),
         type: attr.value.getAttribute("class"),
       });
+      adjustHeight();
     };
     const changeHistory = () => {
       if (!localStorage.getItem("history")) {
@@ -87,22 +89,14 @@ export default {
     };
 
     const restoreHeight = () => attr.value.style = "";
-    const changeHeight = () => {
-      nextTick(() => {
-        let textArea = attr.value;
-        const scrollHeight = textArea.scrollHeight; // 控件所有的高度，包含滚动的那部分(不可见也会有高度)
-        const height = textArea.offsetHeight; // 屏幕上显示的高度
-        if (height <= scrollHeight) {
-          textArea.style.height = "auto"; // 恢复默认值，这个作用就是根据内容自适应textarea高度
-          textArea.style.height = textArea.scrollHeight + "px"; // 拿到最新的高度改变textarea的高度
-        }
-      });
+    const adjustHeight = () => {
+      attr.value.style.height = calcTextareaHeight(attr.value).height;
     };
     return {
       changeHistory,
       change,
       attr,
-      changeHeight,
+      adjustHeight,
       restoreHeight,
     };
   },
